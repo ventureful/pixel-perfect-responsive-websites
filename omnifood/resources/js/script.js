cc572dd5698c88d8e44421589c300ca2e4426211
ventureful <hades0122@outlook.com>
2020-11-11 18:50:54 -0700

    made mobile nav responsive

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -41,25 +41,36 @@ $(document).ready(function () {
     }, {
         offset: '50%'
     });
-    
+
     $('.js--wp-2').waypoint(function () {
         $('.js--wp-2').addClass('animated fadeInUp');
     }, {
         offset: '50%'
     });
-    
+
     $('.js--wp-3').waypoint(function () {
         $('.js--wp-3').addClass('animated fadeIn');
     }, {
         offset: '50%'
     });
-    
+
     $('.js--wp-4').waypoint(function () {
         $('.js--wp-4').addClass('animated pulse');
     }, {
         offset: '50%'
     });
 
+    /*mobile-nav*/
+    $('.js--nav-icon').click(function () {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
 });
-
-
